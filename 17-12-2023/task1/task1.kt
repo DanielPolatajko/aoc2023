78d9a9274127a5f4baec8de968d4e2aa3ff9edbe
DanielPolatajko <daniel@carbonchain.com>
2023-12-20 14:53:37 +0000

    day 17: go make a coffee while it runs

---
+++ 17-12-2023/task1/task1.kt
@@ -0,0 +1,105 @@
+import java.io.File
+import java.io.InputStream
+import kotlin.math.*
+import java.util.PriorityQueue
+
+val compareByDistance = Comparator<Pair<Int, Triple<Int,Int,Char>>> { a,b ->
+    a.first - b.first
+}
+
+fun pathSearch(input: MutableList<MutableList<Int>>) : Int {
+    var unvisited = mutableListOf<Triple<Int,Int, Char>>()
+    var distancesQueue = PriorityQueue<Pair<Int,Triple<Int,Int, Char>>>(compareByDistance)
+    var distances = mutableMapOf<Triple<Int,Int, Char>, Int>()
+    for (i in 0..input.size-1) {
+        for (j in 0..input[0].size-1) {
+            for (d in listOf('H', 'V')) {
+                unvisited.add(Triple(i,j, d))
+                if (i ==0 && j == 0) {
+                    distancesQueue.add(Pair(0, Triple(i,j, d)))
+                    distances[Triple(i,j,d)] = 0
+                } else {
+                    distances[Triple(i,j,d)] = Int.MAX_VALUE
+                }
+            }
+        }
+    }
+    var currentDistance: Int
+    while (!distancesQueue.isEmpty()) {
+
+        var nextInQueue = distancesQueue.remove()
+        var current = nextInQueue.second
+        currentDistance = nextInQueue.first
+
+        if (current !in unvisited) continue
+
+        unvisited = unvisited.filter { it != current }.toMutableList()
+        var x = current.first
+        var y = current.second
+        var weight = currentDistance
+        if (current.third == 'V') {
+            for (step in 1..3) {
+                var newX = x - step
+                if (newX >= 0) {
+                    var node = Triple(newX, y, 'H')
+                    weight += input[newX][y]
+                    if (node in unvisited) {
+                        distancesQueue.add(Pair(weight, node))
+                        distances[node] = min(distances[node]!!, weight)
+                    }
+                }
+            }
+            weight = currentDistance
+            for (step in 1..3) {
+                var newX = x + step
+                if (newX < input.size) {
+                    var node = Triple(newX, y, 'H')
+                    weight += input[newX][y]
+                    if (node in unvisited) {
+                        distancesQueue.add(Pair(weight, node))
+                        distances[node] = min(distances[node]!!, weight)
+                    }
+                }
+            }
+        } else {
+            weight = currentDistance
+            for (step in 1..3) {
+                var newY = y - step
+                if (newY >= 0) {
+                    var node = Triple(x, newY, 'V')
+                    weight += input[x][newY]
+                    if (node in unvisited) {
+                        distancesQueue.add(Pair(weight, node))
+                        distances[node] = min(distances[node]!!, weight)
+                    }
+                }
+            }
+            weight = currentDistance
+            for (step in 1..3) {
+                var newY = y + step
+                if (newY < input[0].size) {
+                    var node = Triple(x, newY, 'V')
+                    weight += input[x][newY]
+                    if (node in unvisited) {
+                        distancesQueue.add(Pair(weight, node))
+                        distances[node] = min(distances[node]!!, weight)
+                    }
+                }
+            }
+        }
+    }
+    return distances.filter { (k, _) -> (k.first == input.size - 1 && k.second == input[0].size - 1) }.values.min()
+}
+
+
+fun main(args: Array<String>) {
+    val inputStream: InputStream = File(args[0]).inputStream()
+    var lineList = mutableListOf<String>()
+
+    inputStream.bufferedReader().forEachLine { lineList.add(it) } 
+
+    var blockList = lineList.map{ it.toList().map { it2 -> it2.toString().toInt() }.toMutableList() }.toMutableList()
+
+    println(pathSearch(blockList))
+
+}
--- Makefile
+++ Makefile
@@ -1,19 +1,19 @@
 build-task:
-	kotlinc $(day)/task$(task)/task$(task).kt -include-runtime -d $(day)/task$(task)/task$(task).jar 
+	kotlinc $(day)-12-2023/task$(task)/task$(task).kt -include-runtime -d $(day)-12-2023/task$(task)/task$(task).jar 
 
 build-day:
 	make build-task day=$(day) task=1
 	make build-task day=$(day) task=2
 
 run-task:
-	java -jar $(day)/task$(task)/task$(task).jar $(day)/task$(task)/$(file)
+	java -jar $(day)-12-2023/task$(task)/task$(task).jar $(day)-12-2023/task$(task)/$(file)
 
 run-day:
-	java -jar $(day)/task1/task1.jar $(day)/task1/example.txt
-	java -jar $(day)/task1/task1.jar $(day)/task1/input.txt
+	java -jar $(day)-12-2023/task1/task1.jar $(day)-12-2023/task1/example.txt
+	java -jar $(day)-12-2023/task1/task1.jar $(day)-12-2023/task1/input.txt
 
-	java -jar $(day)/task2/task2.jar $(day)/task2/example.txt
-	java -jar $(day)/task2/task2.jar $(day)/task2/input.txt
+	java -jar $(day)-12-2023/task2/task2.jar $(day)-12-2023/task2/example.txt
+	java -jar $(day)-12-2023/task2/task2.jar $(day)-12-2023/task2/input.txt
 
 build-and-run-day:
 	make build-day $(day)
